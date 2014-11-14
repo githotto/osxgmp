65bcdb3620504396c6647b5b8a254a8608774f56
Otto van Verseveld <o.h.vanverseveld@goconnectit.nl>
2014-11-14 23:50:59 +0100

    Initial Commit

---
+++ OSXGMP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OSXGMP
+//
+//  Created by Otto van Verseveld on 11/14/14.
+//  Copyright (c) 2014 Otto van Verseveld. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
